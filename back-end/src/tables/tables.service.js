7cebaed8d3a84a84078837fe025df22ea5f96abf
stmiok <128644466+stmiok@users.noreply.github.com>
2023-12-18 16:15:52 -0700

    fixed TextEncoder not defined error

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -14,26 +14,6 @@ function read(table_id) {
   return knex("tables").where({ table_id: table_id }).first();
 }
 
-// function update(table_id, reservation_id) {
-//   return knex
-//     .transaction(async (transaction) => {
-//       await knex("reservations")
-//         .where({ reservation_id })
-//         .update({ status: "seated" })
-//         .transacting(transaction);
-
-//       return knex("tables")
-//         .where({ table_id })
-//         .update({ reservation_id }, "*")
-//         .transacting(transaction)
-//         .then((records) => records[0]);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       throw error;
-//     });
-// }
-
 function update(updatedTable) {
   return knex("tables")
     .select("*")
@@ -42,20 +22,6 @@ function update(updatedTable) {
     .then((updatedRecords) => updatedRecords[0]);
 }
 
-// function finish(table) {
-//   return knex.transaction(async (transaction) => {
-//     await knex("reservations")
-//       .where({ reservation_id: table.reservation_id })
-//       .update({ status: "finished" })
-//       .transacting(transaction);
-
-//     return knex("tables")
-//       .where({ table_id: table.table_id })
-//       .update({ reservation_id: null }, "*")
-//       .transacting(transaction)
-//       .then((records) => records[0]);
-//   });
-// }
 
 function destroy(table_id, reservation_id) {
   return knex.transaction(function (trx) {
@@ -75,6 +41,5 @@ module.exports = {
   create,
   read,
   update,
-  // finish,
   destroy,
 };
