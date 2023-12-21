5a024030d0b3d9b40ddc02264b1dee0e56918c1e
stmiok <128644466+stmiok@users.noreply.github.com>
2023-12-20 18:30:28 -0700

    re-organized migration files to get last test to pass

--- back-end/src/db/migrations/20231204001712_createTablesTable.js
+++ back-end/src/db/migrations/20231204001712_createTablesTable.js
@@ -2,14 +2,14 @@ exports.up = function (knex) {
   return knex.schema.createTable("tables", (table) => {
     table.increments("table_id").primary();
     table.string("table_name").notNullable();
-    table.integer("capacity").unsigned().notNullable();
+    table.integer("capacity").notNullable();
     table.integer("reservation_id").unsigned();
     table
       .foreign("reservation_id")
       .references("reservation_id")
       .inTable("reservations")
       .onDelete("CASCADE");
-    table.timestamp(true, true);
+    table.timestamps(true, true);
   });
 };
 
